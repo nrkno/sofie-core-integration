c0a55de4f06109363d9cda80828b2abbe13a0c4d
Richard Cartwright <spark@streampunk.media>
2020-10-05 11:33:21 +0100

    chore: fix reference to type declaration preventing docs from building

--- src/lib/ddpClient.ts
+++ src/lib/ddpClient.ts
@@ -6,6 +6,7 @@
  *
  * Brought into this project for maintenance reasons, including conversion to Typescript.
  */
+/// <reference types="../faye-websocket" />
 
 import * as WebSocket from 'faye-websocket'
 import * as EJSON from 'ejson'
